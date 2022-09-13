e068ccc76660f91fa74537ce1fc1bb7536ab26f6
Dmitriy Korotkov <54524404+nupellot@users.noreply.github.com>
2022-09-13 15:22:40 +0000

    Got rid of vectors (calloc used instead)

--- main.cpp
+++ main.cpp
@@ -6,36 +6,33 @@ using namespace std;
 // Перечислить все подматрицы, которые можно выбрать из начального фрагмента прямоугольного треугольника Паскаля 
 
 int main() {
-    void PrintSubmatrix(vector<vector<int>>, int, int, int, int);
-    void PrintTriangle(vector<vector<int>>);
-    void HighlightSubmatrix(vector<vector<int>>, int, int, int, int);
+    void PrintSubmatrix(int** Triangle, int width, int height, int LeftDownX, int LeftDownY, int RightUpX, int RightUpY);
+	void HighlightSubmatrix(int** Triangle, int width, int height, int LeftDownX, int LeftDownY, int RightUpX, int RightUpY);
+    void PrintTriangle(int** Triangle, int width, int height);
 
-    // Creating the right pascal triangle.
-    vector<vector<int>> Triangle;
-    // vector<vector<int>> Triangle2(height, vector<int>(width, 0));
+
     int width = 0;
     int height = 0;
     cout << "Input triangle width and height  (Example: 4 6)" << endl;
     cin >> width >> height;
+	int** Triangle = (int**)calloc(height, sizeof(int*));
+	
     for (int i = 0; i < height; i++) {
-        Triangle.emplace_back(vector<int>());
+        Triangle[i] = (int*)calloc(width, sizeof(int));
         for (int j = 0; j < width; j++) {
             if (j > i) {
-                Triangle[i].emplace_back(0);
-            }
-            else
-                if (i == j || j == 0) {
-                    Triangle[i].emplace_back(1);
-                }
-                else {
-                    // Defining value as summ of previous values.
-                    Triangle[i].emplace_back(Triangle[i - 1][j] + Triangle[i - 1][j - 1]);
-                }
+                Triangle[i][j] = 0;
+            } else if (i == j || j == 0) {
+				Triangle[i][j] = 1;
+			} else {
+				// Defining value as summ of previous values.
+				Triangle[i][j] = Triangle[i - 1][j] + Triangle[i - 1][j - 1];
+			}
         }
     }
 
     // Printing the Triangle.
-    PrintTriangle(Triangle);
+    PrintTriangle(Triangle, width, height);
 
     int AmountOfSubmatrixes = 0;
     for (int i = 0; i < height; i++) {
@@ -43,7 +40,7 @@ int main() {
             // Choosing left bottom conner
             for (int k = i; k < height; k++) {
                 for (int l = 0; l <= j; l++) {
-                    HighlightSubmatrix(Triangle, l, k, j, i);
+                    HighlightSubmatrix(Triangle, width, height, l, k, j, i);
                     AmountOfSubmatrixes++;
                 }
             }
@@ -54,7 +51,7 @@ int main() {
 }
 
 
-void PrintSubmatrix(vector<vector<int>> Triangle, int LeftDownX, int LeftDownY, int RightUpX, int RightUpY) {
+void PrintSubmatrix(int** Triangle, int width, int height, int LeftDownX, int LeftDownY, int RightUpX, int RightUpY) {
     cout << "  === Submatrix " << LeftDownX << ";" << LeftDownY << " => " << RightUpX << ";" << RightUpY << " ===" << endl;
     for (int i = RightUpY; i <= LeftDownY; i++) {  // Iterating through rows (Y).
         for (int j = LeftDownX; j <= RightUpX; j++) {   // Iterating through columns (X).
@@ -66,10 +63,10 @@ void PrintSubmatrix(vector<vector<int>> Triangle, int LeftDownX, int LeftDownY, 
 }
 
 // Hightlights given Submatrix in the Triangle
-void HighlightSubmatrix(vector<vector<int>> Triangle, int LeftDownX, int LeftDownY, int RightUpX, int RightUpY) {
+void HighlightSubmatrix(int** Triangle, int width, int height, int LeftDownX, int LeftDownY, int RightUpX, int RightUpY) {
     cout << "=== Submatrix " << LeftDownX << ";" << LeftDownY << " => " << RightUpX << ";" << RightUpY << " ===" << endl;
-    for (int i = 0; i < Triangle.size(); i++) {  // Iterating through rows (Y).
-        for (int j = 0; j < Triangle[i].size(); j++) {   // Iterating through columns (X).
+    for (int i = 0; i < height; i++) {  // Iterating through rows (Y).
+        for (int j = 0; j < width; j++) {   // Iterating through columns (X).
             if (i >= RightUpY && i <= LeftDownY && j >= LeftDownX && j <= RightUpX) {  // Hightligh element if it is included in Submatrix
                 printf("\033[44m%4d\033[0m", Triangle[i][j]);
             }
@@ -82,11 +79,10 @@ void HighlightSubmatrix(vector<vector<int>> Triangle, int LeftDownX, int LeftDow
     cout << endl;
 }
 // Prints Pascal Right Triangle
-void PrintTriangle(vector<vector<int>> Triangle) {
-    cout << "     ===== Pascal Right Triangle " << Triangle[0].size() << "x" << Triangle.size() << " =====" << endl;
-    for (int i = 0; i < Triangle.size(); i++) {
-        for (int j = 0; j < Triangle[i].size(); j++) {
-            // cout << Triangle[i][j] << " ";
+void PrintTriangle(int** Triangle, int width, int height) {
+    cout << "     ===== Pascal Right Triangle " << width << "x" << height << " =====" << endl;
+    for (int i = 0; i < height; i++) {
+        for (int j = 0; j < width; j++) {
             printf("\033[0m%4d\033[0m", Triangle[i][j]);
         }
         cout << endl;
